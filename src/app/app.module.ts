042223a4d8083741b4ecb36d22ad5070f341897a
Ivan Valeff <ivaleff@gmail.com>
2024-04-13 12:25:30 -0300

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { HomeComponent } from './components/home/home.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { HomeComponent } from './components/home/home.component';
     MatMenuModule,
     MatFormFieldModule,
     MatInputModule,
+    MatCardModule
   ],
   providers: [
     provideClientHydration(),
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  categorias: string[] = ['Categoria 1', 'Categoria 2', 'Categoria 3']; // Ejemplo de categorías
+
+  cards = [
+    { 
+      title: 'Card 1',
+      subtitle: 'Subtitle 1',
+      imageUrl: '',
+      content: 'Contenido de la tarjeta 1'
+    },
+    { 
+      title: 'Card 2',
+      subtitle: 'Subtitle 2',
+      imageUrl: '',
+      content: 'Contenido de la tarjeta 2'
+    },
+    { 
+      title: 'Card 3',
+      subtitle: 'Subtitle 3',
+      imageUrl: '',
+      content: 'Contenido de la tarjeta 3'
+    }
+    // Puedes agregar más tarjetas según sea necesario
+  ]; 
 
 }
